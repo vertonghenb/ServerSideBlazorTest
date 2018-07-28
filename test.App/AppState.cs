6e02d1b0b4c94587677e6c867952a38a00caa3fa
Benjamin Vertonghen <ben.vertonghen@gmail.com>
2018-07-28 15:13:49 +0200

    Disposable appstate

--- test.App/AppState.cs
+++ test.App/AppState.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Blazor;
+using System;
 
 namespace test.App
 {
-    public class AppState :  IAppState
+    public class AppState :  IAppState, IDisposable
     {
         public int Count { get; set; }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Remove State");
+        }
     }
 }
